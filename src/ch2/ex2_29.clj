58c250633e2e81a7c36be2ac4b0cd41ef598a33e
Mathias Hörtnagl <m.hoertnagl@gmail.com>
2022-10-04 16:52:20 +0200

    Chapter 2

--- src/ch2/ex2_29.clj
+++ src/ch2/ex2_29.clj
@@ -1,22 +1,60 @@
-(ns ch2.ex2-29 (:require [clojure.test :refer :all]))
+(ns ch2.ex2-29
+  (:require [clojure.test :refer :all]))
 
-(defn fringe [x]
-  (cond (nil? x) nil
-        (not (list? x)) (list x)
-        (empty? x) (list)
-        :else (concat (fringe (first x))
-                      (fringe (rest x)))))
+(defn make-mobile [left right] (list left right))
+(defn make-branch [length structure] (list length structure))
+
+;; a)
+(defn left-branch [mobile] (first mobile))
+(defn right-branch [mobile] (second mobile))
+(defn branch-length [branch] (first branch))
+(defn branch-structure [branch] (second branch))
+
+;; b)
+(declare total-weight-branch)
+
+(defn total-weight [mobile]
+  (+ (total-weight-branch (left-branch mobile))
+     (total-weight-branch (right-branch mobile))))
+
+(defn total-weight-branch [branch]
+  (let [structure (branch-structure branch)]
+    (cond (not (list? structure)) structure
+          (empty? structure) 0
+          :else (total-weight structure))))
+
+;; c)
+(defn mobile-balanced? [mobile]
+  (cond (not (list? mobile)) true
+        :else (let [left-branch (left-branch mobile)
+                    right-branch (right-branch mobile)
+                    left-torque (* (branch-length left-branch)
+                                   (total-weight-branch left-branch))
+                    right-torque (* (branch-length right-branch)
+                                    (total-weight-branch right-branch))]
+                (and (= left-torque right-torque)
+                     (mobile-balanced? (branch-structure left-branch))
+                     (mobile-balanced? (branch-structure right-branch))))))
 
 (deftest tests
-  (testing "fringe"
-    (is (= (fringe nil)
-           nil))
-    (is (= (fringe ())
-           ()))
-    (is (= (fringe (list 1 2 3 4))
-           (list 1 2 3 4)))
-    (is (= (fringe (list (list 1 2) (list 3 4)))
-           (list 1 2 3 4)))
-    (is (= (fringe (list (list (list 1 2) (list 3 4)) (list (list 1 2) (list 3 4))))
-           (list 1 2 3 4 1 2 3 4))))
+  (testing "total weight"
+    (let [x (make-mobile (make-branch 1 4)
+                         (make-branch 1 (make-mobile (make-branch 1 3)
+                                                     (make-branch 1 1))))]
+      (is (= (total-weight x)
+             8))))
+
+  (testing "not balanced"
+    (let [x (make-mobile (make-branch 1 4)
+                         (make-branch 1 (make-mobile (make-branch 1 3)
+                                                     (make-branch 1 1))))]
+      (is (= (mobile-balanced? x)
+             false))))
+
+  (testing "balanced"
+    (let [x (make-mobile (make-branch 1 4)
+                         (make-branch 1 (make-mobile (make-branch 1 3)
+                                                     (make-branch 3 1))))]
+      (is (= (mobile-balanced? x)
+             true))))
 )
