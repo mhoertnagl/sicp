87477f6a8504d862ced2afadd00d2a6f810c6289
Mathias Hörtnagl <m.hoertnagl@gmail.com>
2022-12-29 13:54:24 +0100

    Chapter 2.3.3

--- src/ch2_3/set_ordered.clj
+++ src/ch2_3/set_ordered.clj
@@ -40,7 +40,9 @@
         (empty? set2) set1
         :else (let [x1 (first set1)
                     x2 (first set2)]
-                (cond (= x1 x2)))))
+                (cond (= x1 x2) (cons x1 (union-set (rest set1) (rest set2)))
+                      (< x1 x2) (cons x1 (union-set (rest set1) set2))
+                      :else (cons x2 (union-set set1 (rest set2)))))))
 
 (deftest tests
   (testing "element-of-set?"
