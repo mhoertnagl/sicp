7d9692b46e23d1653d50d3b738be03e9fe3c0b5d
Mathias Hörtnagl <m.hoertnagl@gmail.com>
2023-04-05 14:51:14 +0200

    Chapter 3.3.4

--- src/ch3_3/circuit.clj
+++ src/ch3_3/circuit.clj
@@ -17,12 +17,13 @@
   (agenda-empty? [_] (empty? @queue))
   (first-agenda-item [_] (second (first @queue)))
   (remove-first-agenda-item! [_]
-    (swap! queue rest))
+    (swap! queue (fn [elems] (into (sorted-map) (rest elems)))))
   (add-to-agenda! [_ delay action]
     (swap! queue assoc (+ @time delay) action))
   (current-time [_] @time))
 
-(defn make-agenda [] (->Agenda (atom 0) (atom (sorted-map))))
+(defn make-agenda []
+  (->Agenda (atom 0) (atom (sorted-map))))
 
 (defn propagate [agenda]
   (while (not (agenda-empty? agenda))
@@ -30,45 +31,42 @@
       (first-item)
       (remove-first-agenda-item! agenda))))
 
-(def the-agenda (make-agenda))
-
-; TODO: Unsatisfactory. Couples the circuit to a global agenda instance.
-(defn after-delay [delay action]
-  (add-to-agenda! the-agenda delay action))
-
 (defprotocol IWire
   (get-signal [wire])
   (set-signal! [wire new-value])
   (add-action! [wire action]))
 
-(defrecord Wire [value actions]
+(defrecord Wire [agenda value actions]
   IWire
   (get-signal [_] @value)
   (set-signal! [_ new-value]
     (reset! value new-value)
-    (doseq [action @actions] (action)))
+    (doseq [action @actions] (action agenda)))
   (add-action! [_ action]
     (swap! actions conj action)
-    (action)))
+    (action agenda)))
 
-(defn make-wire [] (->Wire (atom 0) (atom [])))
+(defn make-wire [agenda]
+  (->Wire agenda (atom 0) (atom [])))
 
 (defn not-gate [input output]
   (letfn [(logical-not [s] (if (= s 0) 1 0))
-          (action []
+          (action [agenda]
             (let [new-value (logical-not (get-signal input))]
-              (after-delay NOT-GATE-DELAY
-                           (fn [] (set-signal! output new-value)))))]
+              (add-to-agenda! agenda
+                              NOT-GATE-DELAY
+                              (fn [] (set-signal! output new-value)))))]
     (add-action! input action)
     :ok))
 
 (defn and-gate [a1 a2 output]
   (letfn [(logical-and [s1 s2] (if (and (= s1 1) (= s2 1)) 1 0))
-          (action []
+          (action [agenda]
             (let [new-value (logical-and (get-signal a1)
                                          (get-signal a2))]
-              (after-delay AND-GATE-DELAY
-                           (fn [] (set-signal! output new-value)))))]
+              (add-to-agenda! agenda
+                              AND-GATE-DELAY
+                              (fn [] (set-signal! output new-value)))))]
     (add-action! a1 action)
     (add-action! a2 action)
     :ok))
@@ -76,11 +74,12 @@
 ; Ex 3.28
 (defn or-gate [a1 a2 output]
   (letfn [(logical-or [s1 s2] (if (or (= s1 1) (= s2 1)) 1 0))
-          (action []
+          (action [agenda]
             (let [new-value (logical-or (get-signal a1)
                                         (get-signal a2))]
-              (after-delay OR-GATE-DELAY
-                           (fn [] (set-signal! output new-value)))))]
+              (add-to-agenda! agenda
+                              OR-GATE-DELAY
+                              (fn [] (set-signal! output new-value)))))]
     (add-action! a1 action)
     (add-action! a2 action)
     :ok))
@@ -89,36 +88,121 @@
 ; Use the identity: A ∨ B ≡ ¬(¬A ∧ ¬B)
 
 ; Ex 3.30
+(defn make-wires [agenda n]
+  (for [_ (range n)] (make-wire agenda)))
 
+(defn get-signals [wires]
+  (map get-signal wires))
 
-(defn half-adder [a b s c]
-  (let [d (make-wire)
-        e (make-wire)]
+(defn set-signals! [wires new-values]
+  (map set-signal! wires new-values))
+
+(defn half-adder [agenda a b s c]
+  (let [d (make-wire agenda)
+        e (make-wire agenda)]
     (or-gate a b d)
     (and-gate a b c)
     (not-gate c e)
     (and-gate d e s)))
 
-(defn full-adder [a b c-in sum c-out]
-  (let [s (make-wire)
-        c1 (make-wire)
-        c2 (make-wire)]
-    (half-adder b c-in s c1)
-    (half-adder a s sum c2)
+(defn full-adder [agenda a b c-in sum c-out]
+  (let [s (make-wire agenda)
+        c1 (make-wire agenda)
+        c2 (make-wire agenda)]
+    (half-adder agenda b c-in s c1)
+    (half-adder agenda a s sum c2)
     (or-gate c1 c2 c-out)))
 
 (deftest tests
-  (testing "set signal"
-    (let [wire (make-wire)]
+  (testing "set signal without actions"
+    (let [agenda (make-agenda)
+          wire (make-wire agenda)]
       (is (= (get-signal wire) 0))
       (set-signal! wire 1)
-      (is (= (get-signal wire) 1)))
-    )
+      (is (= (get-signal wire) 1))
+      (set-signal! wire 0)
+      (is (= (get-signal wire) 0))
+      ))
+
+  (testing "set signal with actions"
+    (let [agenda (make-agenda)
+          wire (make-wire agenda)]
+      (add-action! wire (fn [agenda] 0))
+      (is (= (get-signal wire) 0))
+      (set-signal! wire 1)
+      (is (= (get-signal wire) 1))
+      (set-signal! wire 0)
+      (is (= (get-signal wire) 0))
+      ))
 
   (testing "test not-gate"
-    (let [input (make-wire)
-          output (make-wire)]
+    (let [agenda (make-agenda)
+          input (make-wire agenda)
+          output (make-wire agenda)]
       (not-gate input output)
-      (is (= (first @my-map) [2 3])))
-    )
+
+      (set-signal! input 0)
+      (propagate agenda)
+      (is (= (get-signal output) 1))
+
+      (set-signal! input 1)
+      (propagate agenda)
+      (is (= (get-signal output) 0))
+      ))
+
+  (testing "test and-gate"
+    (let [agenda (make-agenda)
+          input1 (make-wire agenda)
+          input2 (make-wire agenda)
+          output (make-wire agenda)]
+      (and-gate input1 input2 output)
+
+      (set-signal! input1 0)
+      (set-signal! input2 0)
+      (propagate agenda)
+      (is (= (get-signal output) 0))
+
+      (set-signal! input1 0)
+      (set-signal! input2 1)
+      (propagate agenda)
+      (is (= (get-signal output) 0))
+
+      (set-signal! input1 1)
+      (set-signal! input2 0)
+      (propagate agenda)
+      (is (= (get-signal output) 0))
+
+      (set-signal! input1 1)
+      (set-signal! input2 1)
+      (propagate agenda)
+      (is (= (get-signal output) 1))
+      ))
+
+  (testing "test or-gate"
+    (let [agenda (make-agenda)
+          input1 (make-wire agenda)
+          input2 (make-wire agenda)
+          output (make-wire agenda)]
+      (or-gate input1 input2 output)
+
+      (set-signal! input1 0)
+      (set-signal! input2 0)
+      (propagate agenda)
+      (is (= (get-signal output) 0))
+
+      (set-signal! input1 0)
+      (set-signal! input2 1)
+      (propagate agenda)
+      (is (= (get-signal output) 1))
+
+      (set-signal! input1 1)
+      (set-signal! input2 0)
+      (propagate agenda)
+      (is (= (get-signal output) 1))
+
+      (set-signal! input1 1)
+      (set-signal! input2 1)
+      (propagate agenda)
+      (is (= (get-signal output) 1))
+      ))
   )
